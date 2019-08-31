49bb4ac544e6177ac4e8c79687dde10ad63a688c
Carl Hilber <carl.hilber@my.jcu.edu.au>
2019-08-31 18:12:06 +1000

    commit

--- Prac_4/part_1.py
+++ Prac_4/part_1.py
@@ -1,1 +1,17 @@
 
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# What values do the following expressions have?
+# the numbers are 0, 1, 2, 3, 4, 5 and 6
+
+
+# Question 1
+numbers[0] = "ten"
+# Question 2
+numbers[-1] = 1
+# Question 3
+numbers[2:]
+# Question 4
+9 in numbers
+
+
